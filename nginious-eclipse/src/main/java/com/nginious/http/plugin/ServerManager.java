c19347590994bd5032656ebcb8e92f59969e4974
Bojan Pisler <bojan.pisler@netdigital.se>
2012-12-23 11:06:59 +0100

    - Added tracking of changes to Nginiout plugin to automatically update nginious-api.jar if needed

--- nginious-eclipse/src/main/java/com/nginious/http/plugin/ServerManager.java
+++ nginious-eclipse/src/main/java/com/nginious/http/plugin/ServerManager.java
@@ -17,6 +17,8 @@
 package com.nginious.http.plugin;
 
 import java.io.IOException;
+import java.net.URL;
+import java.util.ArrayList;
 import java.util.Collection;
 import java.util.concurrent.ConcurrentHashMap;
 
@@ -29,6 +31,13 @@ import org.eclipse.core.resources.IResourceDelta;
 import org.eclipse.core.resources.ResourcesPlugin;
 import org.eclipse.core.runtime.CoreException;
 import org.eclipse.core.runtime.IPath;
+import org.eclipse.core.runtime.IProgressMonitor;
+import org.eclipse.core.runtime.NullProgressMonitor;
+import org.eclipse.core.runtime.Path;
+import org.eclipse.jdt.core.IClasspathEntry;
+import org.eclipse.jdt.core.IJavaProject;
+import org.eclipse.jdt.core.JavaCore;
+import org.eclipse.jdt.core.JavaModelException;
 import org.eclipse.jface.text.Document;
 
 import com.nginious.http.application.ApplicationException;
@@ -45,11 +54,14 @@ class ServerManager implements IResourceChangeListener {
 	
 	private ConcurrentHashMap<String, HttpServerEnvironment> servers;
 	
+	private Logger logger;
+	
 	private ServerManager() {
 		super();
 		this.servers = new ConcurrentHashMap<String, HttpServerEnvironment>();
 		int eventMask = IResourceChangeEvent.POST_CHANGE | IResourceChangeEvent.PRE_CLOSE | IResourceChangeEvent.PRE_DELETE;
 		ResourcesPlugin.getWorkspace().addResourceChangeListener(this, eventMask);
+		this.logger = new Logger("/tmp/nginious.out");
 	}
 	
 	static ServerManager getInstance() {
@@ -64,6 +76,8 @@ class ServerManager implements IResourceChangeListener {
 	}
 	
 	public void resourceChanged(IResourceChangeEvent event) {
+		logger.log("ENTER ServerManager.resourceChanged event={0}", event);
+		
 		int type = event.getType();
 		
 		switch(type) {
@@ -93,26 +107,36 @@ class ServerManager implements IResourceChangeListener {
 			}
 			break;
 		}
+		
+		logger.log("EXIT ServerManager.resourceChanged");
 	}
 	
 	private boolean projectHasChanged(IProject project) {
+		logger.log("ENTER ServerManager.projectHasChanged project={0}", project);
 		HttpServerEnvironment env = servers.get(project.getName());
 		
 		if(env != null) {
-			return env.hasChanged();
+			boolean changed = env.hasChanged();
+			logger.log("EXIT ServerManager.projectHasChanged changed={0}", changed);
+			return changed;
 		}
 		
 		// Verify that project has been fully created
 		IFolder folder = project.getFolder("WebContent");
 		
 		if(!folder.exists()) {
+			logger.log("EXIT ServerManager.projectHasChanged changed=false");
 			return false;
 		}
 		
-		return project.isOpen();
+		boolean open = project.isOpen();
+		logger.log("EXIT ServerManager.projectHAsChanged open={0}", open);
+		return open;
 	}
 	
 	void stopServer(IProject project) {
+		logger.log("ENTER ServerManager.stopServer project={0}", project);
+		
 		try {
 			HttpServerEnvironment env = servers.remove(project.getName());
 			
@@ -120,17 +144,69 @@ class ServerManager implements IResourceChangeListener {
 				HttpServer server = env.getServer();
 				server.stop();
 			}
+			
+			logger.log("EXIT SeverManager.stopServer");
 		} catch(IOException e) {
+			logger.log("ServerManager.stopServer exception");
 			// Do nothing
 		}
 	}
 	
 	void restartServer(IProject project) {
+		logger.log("ENTER ServerManager.restartServer project={0}", project);
 		stopServer(project);
 		startServer(project);
+		logger.log("EXIT ServerManager.restartServer");
+	}
+	
+	boolean updateProjectWithPluginVersion(IProject project) {
+		logger.log("ENTER ServerManager.updateProjectWithPluginVersion project={0}", project);
+		
+		try {
+			URL apiJar = NginiousPlugin.getApiJar();
+			String filePath = apiJar.toString();
+			filePath = filePath.substring(5);
+			Path apiJarPath = new Path(filePath);
+			
+			IJavaProject javaProject = JavaCore.create(project);
+			IClasspathEntry[] entries = javaProject.getRawClasspath();
+			ArrayList<IClasspathEntry> newEntries = new ArrayList<IClasspathEntry>();
+			boolean changed = false;
+			// javaProject.save
+			
+			for(IClasspathEntry entry : entries) {
+				if(entry.getEntryKind() == IClasspathEntry.CPE_LIBRARY) {
+					IPath path = entry.getPath();
+					
+					if(path.lastSegment() != null && path.lastSegment().endsWith("nginious-api.jar")) {
+						changed = true;
+						entry = JavaCore.newLibraryEntry(apiJarPath, null, null);						
+					}
+				}
+				
+				newEntries.add(entry);
+			}
+			
+			if(changed) {
+				IProgressMonitor progress = new NullProgressMonitor();
+				entries = newEntries.toArray(new IClasspathEntry[newEntries.size()]);
+				javaProject.setRawClasspath(entries, progress);
+				javaProject.save(progress, true);
+			}
+			
+			logger.log("EXIT ServerManager.updateProjectWithPluginVersion changed={0}", changed);
+			return changed;
+		} catch(JavaModelException e) {
+			logger.log("ServerManager.updateProkectWithPluginVersion exception", e);
+			return false;
+		} catch(IOException e) {
+			logger.log("ServerManager.updateProkectWithPluginVersion exception", e);
+			return false;
+		}
 	}
 	
 	void startServer(IProject project) {
+		logger.log("ENTER ServerManager.startServer project={0}", project);
 		int listenPort = NginiousPlugin.DEFAULT_LISTEN_PORT;
 		String publishUrl = NginiousPlugin.DEFAULT_PUBLISH_URL;
 		String publishUsername = NginiousPlugin.DEFAULT_PUBLISH_USERNAME;
@@ -150,10 +226,14 @@ class ServerManager implements IResourceChangeListener {
 			String title = Messages.ServerManager_listen_port_error_title;
 			String message = Messages.ServerManager_listen_port_error_message + " " + project.getName();
 			MessagesUtils.displayMessageDialog(e.getMessage(), null, title, message);
+			logger.log("ServerManager.startServer exception", e);
 		} catch(CoreException e) {
 			String title = Messages.ServerManager_properties_error_title;
 			String message = Messages.ServerManager_listen_port_error_message + " " + project.getName();
 			MessagesUtils.perform(e, null, title, message);
+			logger.log("ServerManager.startServer exception", e);
+		} catch(Throwable t) {
+			logger.log("ServerManager.startServer exception", t);
 		}
 		
 		try {
@@ -179,54 +259,70 @@ class ServerManager implements IResourceChangeListener {
 			env.setPublishUsername(publishUsername);
 			env.setPublishPassword(publishPassword);
 			servers.put(project.getName(), env);
+			logger.log("EXIT ServerManager.startServer");
 		} catch(ApplicationException e) {
 			String title = Messages.ServerManager_server_error_title;
 			String message = Messages.ServerManager_server_error_message + " " + project.getName();
-			MessagesUtils.displayMessageDialog(e.getMessage(), null, title, message);			
+			MessagesUtils.displayMessageDialog(e.getMessage(), null, title, message);
+			logger.log("ServerManager.startServer exception", e);
 		} catch(IOException e) {
 			String title = Messages.ServerManager_server_error_title;
 			String message = Messages.ServerManager_server_error_message + " " + project.getName();
 			MessagesUtils.displayMessageDialog(e.getMessage(), null, title, message);
+			logger.log("ServerManager.startServer exception", e);
+		} catch(Throwable t) {
+			logger.log("ServerManager.startServer exception", t);
 		}
 	}
 	
 	IProject checkListenPortUsage(int listenPort) {
+		logger.log("ENTER ServerManager.checkListenPortUsage listenPort={0}", listenPort);
 		Collection<HttpServerEnvironment> envs = servers.values();
 		
 		for(HttpServerEnvironment env : envs) {
 			if(env.getPort() == listenPort) {
-				return env.getProject();
+				IProject project = env.getProject();
+				logger.log("EXIT ServerMamager.checkListenPortUsage project={0}", project);
 			}
 		}
 		
+		logger.log("EXIT ServerManager.checkListenPortUsage project=null");
 		return null;
 	}
 	
 	Document getMessageLogDocument(IProject project) {
+		logger.log("ENTER ServerManager.getMessageLogDocument project=", project);
 		HttpServerEnvironment env = servers.get(project.getName());
 		
 		if(env != null) {
 			LogViewConsumer consumer = env.getMessageLogConsumer();
 			
 			if(consumer != null) {
-				return consumer.getDocument();
+				Document doc =  consumer.getDocument();
+				logger.log("EXIT ServerManager.getMessageLogDocument document={0}", doc);
+				return doc;
 			}
 		}
 		
+		logger.log("EXIT ServerManager.getMessageLogDocument document=null");
 		return null;
 	}
 	
 	Document getAccessLogDocument(IProject project) {
+		logger.log("ENTER ServerManager.getAccessLogDocument project={0}", project);
 		HttpServerEnvironment env = servers.get(project.getName());
 		
 		if(env != null) {
 			LogViewConsumer consumer = env.getAccessLogConsumer();
 			
 			if(consumer != null) {
-				return consumer.getDocument();
+				Document doc = consumer.getDocument();
+				logger.log("EXIT ServerManager.getAccessLogDocument document={0}", doc);
+				return doc;
 			}
 		}
 		
+		logger.log("EXIT ServerManager.getAccessLogDocument document=null");
 		return null;
 	}
 	
